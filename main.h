083ee618c8214bd080c2b5a3cb48a4b5922f5c54
Mikias <mikiasabiy16@gmail.com>
2022-10-07 21:35:38 +0000

    add: main.h: main header file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* local defination and declarations */
+int _check_pass(char *str);
+void _help_message(void);
+int add (char *name);
+int rem (unsigned int id);
+int info(unsigned int id);
+
+#endif
